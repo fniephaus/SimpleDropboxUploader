21d22f3a3afdf9eacf7112f444e139fc9112eca2
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-01-15 18:11:07 +0100

    Error handling improved

--- js/ready.js
+++ js/ready.js
@@ -7,15 +7,18 @@ $(function () {
     $('#fileupload').fileupload({
         url: 'upload_file.php',
         dataType: 'json',
+        submit: function (e, data) {
+            data.formData = {access_code: $('#access_code').val(), id: upload_count};
+        },
         add: function (e, data) {
             upload_count++;
             if(data.files.length>0){
                 $("#uploads").show();
                 $("#upload_count").text(upload_count);
                 $.each(data.files, function (index, file) {
-                    var row = '<tr class="file_' + convertFilename(file.name) + '">' +
+                    var row = '<tr id="file_' + upload_count + '">' +
                     '<td class="process_count text-center">' + upload_count + '</td>' +
-                    '<td>' + file.name + '</td>' +
+                    '<td id="filename_' + upload_count + '">/' + file.name + '</td>' +
                     '<td class="process_status"><span class="label label-warning pull-right">Processing</span></td>' +
                     '</tr>';
                     $('#file_list tbody').append(row);
@@ -24,27 +27,34 @@ $(function () {
             data.submit();
         },
         done: function (e, data) {
-            if(data.files.length>0){
-                $.each(data.files, function (index, file) {
-                    var label = $('.file_' + convertFilename(file.name) + ' td .label');
+            if(data.result.files && data.result.files.length>0){
+                $.each(data.result.files, function (index, file) {
+                    $('#filename_' + file.id).text(file.name);
+                    var label = $('#file_' + file.id + ' td .label');
                     label.removeClass('label-warning');
-                    label.addClass('label-success');
-                    label.text('Done');
+                    if(file.error){
+                        console.log(file.error);
+                        label.addClass('label-danger');
+                        label.html('Failed');
+                    } else {
+                        label.addClass('label-success');
+                        label.text('Done');
+                    }
                     done_count++;
                 });
-                checkAllDone();
-            }
-        },
-        fail: function (e, data) {
-            if(data.files.length>0){
-                $.each(data.files, function (index, file) {
-                    var label = $('.file_' + convertFilename(file.name) + ' td .label');
-                    label.removeClass('label-warning');
-                    label.addClass('label-danger');
-                    label.text('Failed');
-                    done_count++;
-                });
-                checkAllDone();
+                if(upload_count==done_count){
+                    $("#progress").removeClass('active');
+                    $("#progress").removeClass('progress-striped');
+                    $("#upload_status").text('Upload done!');
+                }
+            }else if(data.result.error){
+                $("#progress").removeClass('active');
+                $("#progress").removeClass('progress-striped');
+                $("#upload_status").text('Upload failed!');
+                var label = $('.process_status .label');
+                label.removeClass('label-warning');
+                label.addClass('label-danger');
+                label.html('Failed');
             }
         },
         start:  function (e, data) {
@@ -99,16 +109,8 @@ $(function () {
 
 });
 
-function checkAllDone(){
-    if(upload_count==done_count){
-        $("#progress").removeClass('active');
-        $("#progress").removeClass('progress-striped');
-        $("#upload_status").text('Upload done!');
-    }
-}
-
 function convertFilename(filename) {
-    return filename.trim().replace(/[^a-z0-9]+/gi, '-');
+    return filename.trim().replace("/","").replace(/[^a-z0-9]+/gi, '-');
 }
 
 function formatBitrate(bits) {
--- upload_file.php
+++ upload_file.php
@@ -5,56 +5,54 @@ use \Dropbox as dbx;
 
 header('Content-type: application/json');
 
-if ((empty($password) || (isset($_POST['password']) && $_POST['password']==$password)) && isset($_FILES['files'])){
+if ((empty($access_code) || (isset($_POST['access_code']) && $_POST['access_code']==$access_code)) &&
+    isset($_POST['id']) && is_numeric($_POST['id']) &&
+    isset($_FILES['files'])){
 
     try {
         list($accessToken, $host) = dbx\AuthInfo::loadFromJsonFile("./config/dropbox.json");
+
+        $locale = null;
+        $client = new dbx\Client($accessToken, "upload-file", $locale, $host);
+
+        if (is_uploaded_file($_FILES['files']['tmp_name'][0])) {
+           $sourcePath = $_FILES['files']['tmp_name'][0];
+           $dropboxPath = "/". $_FILES['files']['name'][0];
+        } else {
+            print '{"files":[{"id":'.$_POST['id'].',"name":"","size":"","error":"An upload error occured."]}';
+            die;
+        }
+
+        $pathError = dbx\Path::findErrorNonRoot($dropboxPath);
+        if ($pathError !== null) {
+            print '{"files":[{"id":'.$_POST['id'].',"name":"'.$dropboxPath.'","size":"","error":"Invalid <dropbox-path>: '.$pathError.'"]}';
+            die;
+        }
+
+        $size = null;
+        if (\stream_is_local($sourcePath)) {
+            $size = \filesize($sourcePath);
+        }
+
+        $fp = fopen($sourcePath, "rb");
+        $metadata = $client->uploadFile($dropboxPath, dbx\WriteMode::add(), $fp, $size);
+        fclose($fp);
+
+        if($logging){
+            $logfile = tmpfile();
+            fwrite($logfile, "Upload date: " . date("F j, Y, g:i a") . "\nIP: " . $_SERVER['REMOTE_ADDR']);
+            fseek($logfile, 0);
+            $logsize = filesize(stream_get_meta_data($logfile)['uri']);
+            $log_metadata = $client->uploadFile($dropboxPath.".log", dbx\WriteMode::add(), $logfile, $logsize);
+            fclose($logfile);
+        }
+
+        print '{"files":[{"id":'.$_POST['id'].',"name":"'.$metadata['path'].'","size":'.$size.'}]}';
+        die;
     } catch (dbx\AuthInfoLoadException $ex) {
-        //print_r($ex);
-        print '{"files":[]}';
+        print '{"files":[{"id":'.$_POST['id'].',"name":"'.$metadata['path'].'","size":'.$size.',"error":"An authentication problem occured.}]}';
         die;
     }
-
-    $locale = null;
-    $client = new dbx\Client($accessToken, "upload-file", $locale, $host);
-
-    if (is_uploaded_file($_FILES['files']['tmp_name'][0])) {
-       $sourcePath = $_FILES['files']['tmp_name'][0];
-       $dropboxPath = "/". $_FILES['files']['name'][0];
-    } else {
-        //print "Upload error";
-        print '{"files":[]}';
-        die;
-    }
-
-    $pathError = dbx\Path::findErrorNonRoot($dropboxPath);
-    if ($pathError !== null) {
-        //print "Invalid <dropbox-path>: $pathError\n";
-        print '{"files":[]}';
-        die;
-    }
-
-    $size = null;
-    if (\stream_is_local($sourcePath)) {
-        $size = \filesize($sourcePath);
-    }
-
-    $fp = fopen($sourcePath, "rb");
-    $metadata = $client->uploadFile($dropboxPath, dbx\WriteMode::add(), $fp, $size);
-    fclose($fp);
-
-    if($logging){
-        $logfile = tmpfile();
-        fwrite($logfile, "Upload date: " . date("F j, Y, g:i a") . "\nIP: " . $_SERVER['REMOTE_ADDR']);
-        fseek($logfile, 0);
-        $logsize = filesize(stream_get_meta_data($logfile)['uri']);
-        $log_metadata = $client->uploadFile($dropboxPath.".log", dbx\WriteMode::add(), $logfile, $logsize);
-        fclose($logfile);
-    }
-
-    print '{"files":[{"name":"'.$dropboxPath.'","size":'.$size.'}]}';
-    die;
 }
 
-print '{"files":[]}';
-die;
+print '{"error":"Upload failed"}';
